e26f1e693a76632acaf4ffa16cbdaf9c0da23d92
Koghtev <Koghtev1@YANDEX.RU>
2015-07-05 15:58:32 +0300

    files

---
+++ Practice/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  Practice
+//
+//  Created by Вадим Кохтев on 05/07/15.
+//  Copyright (c) 2015 Вадим Кохтев. All rights reserved.
+//
+
+
+
+#include <iostream>
+
+int socket(int AF_INET, int SOCK_STREAM, int n){
+    
+    return 0;
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
